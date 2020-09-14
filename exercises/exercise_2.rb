b57a5fdef27be57b605914f5df0e7b0f7ec6afa1
Jenny Citherlet <jennycith@gmail.com>
2020-09-14 23:39:26 +0000

    Validate for both model - Exercise 7

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,11 +1,11 @@
 require_relative '../setup'
 require_relative './exercise_1'
-
 puts "Exercise 2"
 puts "----------"
+
 
 @store1 = Store.find_by(id: 1);
 @store2 = Store.find_by(id: 2);
 
-@store1.update(name: 'Metrotown');
+@store1.update(name: 'Greendale');
 puts @store1.name;
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Anne", last_name: "Brown", hourly_rate: 85)
+@store1.employees.create(first_name: "Jack", last_name: "Smith", hourly_rate: 45)
+@store1.employees.create(first_name: "Chris", last_name: "Lamb", hourly_rate: 75)
+
+@store2.employees.create(first_name: "Corinne", last_name: "Anthony", hourly_rate: 65)
+@store2.employees.create(first_name: "Alfred", last_name: "Grenn", hourly_rate: 75)
+@store2.employees.create(first_name: "Bill", last_name: "Ortner", hourly_rate: 95)
